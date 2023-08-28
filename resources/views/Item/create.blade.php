b49a2c78348c79f10cf06c42d3592bdfbc9a2387
Farhan Nur Fuadi <farhannfs5121@gmail.com>
2023-08-28 08:25:59 +0700

    + item : file upload

--- resources/views/Item/create.blade.php
+++ resources/views/Item/create.blade.php
@@ -7,6 +7,8 @@
     <link href="https://cdn.datatables.net/1.13.1/css/dataTables.bootstrap5.min.css" rel="stylesheet">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/select2@4.0.13/dist/css/select2.min.css" />
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/select2-bootstrap-5-theme@1.3.0/dist/select2-bootstrap-5-theme.min.css" />
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/dropzone/5.9.3/dropzone.min.css" type="text/css" />
+    <meta name="csrf-token" content="{{ csrf_token() }}" />
     <style>
         .input-tags {
             width: 100%;
@@ -28,6 +30,11 @@
             padding: 16px !important;
             background: transparent;
         }
+        #image-upload {
+            border-radius: 5px;
+            border: 3px dashed #e5e5e5;
+            background-color: #f7f7f7;
+        }
     </style>
 @endpush
 
@@ -42,108 +49,103 @@
                 "Create" => "#",
             ],
         ])
-        <form action="{{route('item.store')}}" method="post">
+        <form action="{{route('item.store')}}" method="post" id="itemForm">
             @csrf
-            <div class="form-elements-wrapper">
-                <div class="row">
-                    <div class="col-lg-12">
-                        <!-- input style start -->
-                        <div class="card-style mb-30">
-                            <div class="row">
+        </form>
+        <div class="form-elements-wrapper">
+            <div class="row">
+                <div class="col-lg-12">
+                    <!-- input style start -->
+                    <div class="card-style mb-30">
+                        <div class="row">
 
-                                <div class="col-lg-6">
-                                    <div class="input-style-1">
-                                        <label>Item Code</label>
-                                        <input type="text" placeholder="Item Code" name="Code" required />
-                                        @error('Code') <span class="text-danger">{{$message}}</span> @enderror
-                                    </div>
+                            <div class="col-lg-6">
+                                <div class="input-style-1">
+                                    <label>Item Code</label>
+                                    <input type="text" placeholder="Item Code" form="itemForm" name="Code" required />
+                                    @error('Code') <span class="text-danger">{{$message}}</span> @enderror
                                 </div>
+                            </div>
 
-                                <div class="col-lg-6">
-                                    <div class="input-style-1">
-                                        <label>Item Name</label>
-                                        <input type="text" placeholder="Item Name" name="Name" required />
-                                        @error('Name') <span class="text-danger">{{$message}}</span> @enderror
-                                    </div>
+                            <div class="col-lg-6">
+                                <div class="input-style-1">
+                                    <label>Item Name</label>
+                                    <input type="text" placeholder="Item Name" name="Name" form="itemForm" required />
+                                    @error('Name') <span class="text-danger">{{$message}}</span> @enderror
                                 </div>
+                            </div>
 
-                                <div class="col-lg-6">
-                                    <div class="input-style-1">
-                                        <label>Unit</label>
-                                        <input type="text" placeholder="Unit" name="Unit" required />
-                                        @error('Unit') <span class="text-danger">{{$message}}</span> @enderror
-                                    </div>
+                            <div class="col-lg-6">
+                                <div class="input-style-1">
+                                    <label>Unit</label>
+                                    <input type="text" placeholder="Unit" name="Unit" form="itemForm" required />
+                                    @error('Unit') <span class="text-danger">{{$message}}</span> @enderror
                                 </div>
+                            </div>
 
-                                <div class="col-lg-6">
-                                    <div class="select-sm select-style-1">
-                                        <label>Category Item</label>
-                                        <div class="select-position input-tags">
-                                            <select class="js-example-basic-single form-" id="tags" multiple name="Category[]">
-                                                @foreach($category as $c)
-                                                    <option value="{{$c->CategoryId}}">{{$c->Name}}</option>
-                                                @endforeach
-                                            </select>
-                                            @error('Category') <span class="text-danger">{{$message}}</span> @enderror
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <div class="col-lg-6">
-                                    <div class="select-sm select-style-1">
-                                        <label>Status of Item </label>
-                                        <div class="select-position">
-                                            <select class="light-bg" name="Status" required>
-                                                <option selected disabled>Choose Status Item</option>
-                                                <option value="1">Active</option>
-                                                <option value="0">Nonactive</option>
-                                            </select>
-                                            @error('Status') <span class="text-danger">{{$message}}</span> @enderror
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <div class="col-lg-6">
-                                    <div class="select-sm select-style-1">
-                                        <label>Item Behavior</label>
-                                        <div class="select-position">
-                                            <select class="light-bg" id="Filter-Item" name="ItemBehavior" required>
-                                                <option selected disabled>Choose Item Behavior</option>
-                                                <option value="1">Hour Usage Monitor</option>
-                                                <option value="2">Consumable</option>
-                                                {{-- <option value="3">Non Consumable</option> --}}
-                                            </select>
-                                            @error('ItemBehavior') <span class="text-danger">{{$message}}</span> @enderror
-                                        </div>
-                                    </div>
-                                </div>
-
-                                <div class="col-lg-6">
-                                    <div class="input-style-1 d-none" id="Alert-Hour">
-                                        <label>Alert Hour Maintenance</label>
-                                        <input type="number" id="alert-input" placeholder="Hour Usage Monitor (in Hour)" name="AlertHourMaintenance" />
-                                    </div>
-                                    <div class="input-style-1 d-none" id="Alert-Consumable">
-                                        <label>Consumable</label>
-                                        <input type="number" id="alert-input" placeholder="Consumable (in Unit)" name="AlertConsumable" />
+                            <div class="col-lg-6">
+                                <div class="select-sm select-style-1">
+                                    <label>Category Item</label>
+                                    <div class="select-position input-tags">
+                                        <select class="js-example-basic-single form-" id="tags" multiple name="Category[]" form="itemForm">
+                                            @foreach($category as $c)
+                                                <option value="{{$c->CategoryId}}">{{$c->Name}}</option>
+                                            @endforeach
+                                        </select>
+                                        @error('Category') <span class="text-danger">{{$message}}</span> @enderror
                                     </div>
                                 </div>
                             </div>
-                            <!-- End Row -->
-                            <div class="row">
-                                <div class="col-lg-12 text-end">
-                                    <a href="{{route('item.index')}}" class="btn btn-secondary">Back</a>
-                                    <button type="submit" class="btn btn-primary">Submit</button>
+
+                            <div class="col-lg-6">
+                                <div class="select-sm select-style-1">
+                                    <label>Item Behavior</label>
+                                    <div class="select-position">
+                                        <select class="light-bg" id="Filter-Item" name="ItemBehavior" form="itemForm" required>
+                                            <option selected disabled>Choose Item Behavior</option>
+                                            <option value="1">Hour Usage Monitor</option>
+                                            <option value="2">Consumable</option>
+                                            {{-- <option value="3">Non Consumable</option> --}}
+                                        </select>
+                                        @error('ItemBehavior') <span class="text-danger">{{$message}}</span> @enderror
+                                    </div>
+                                </div>
+                            </div>
+
+                            <div class="col-lg-6">
+                                <div class="input-style-1 d-none" id="Alert-Hour">
+                                    <label>Alert Hour Maintenance</label>
+                                    <input type="number" id="alert-input" placeholder="Hour Usage Monitor (in Hour)" name="AlertHourMaintenance" form="itemForm" />
+                                </div>
+                                <div class="input-style-1 d-none" id="Alert-Consumable">
+                                    <label>Consumable</label>
+                                    <input type="number" id="alert-input" placeholder="Consumable (in Unit)" name="AlertConsumable" form="itemForm" />
+                                </div>
+                            </div>
+
+                            <div class="col-lg-6">
+                                <div class="input-style-1">
+                                    <label>Upload Your File</label>
+                                    <form action="{{route('dropzone.store')}}" method="post" name="file" files="true" enctype="multipart/form-data" class="dropzone" id="image-upload">
+                                        @csrf
+                                    </form>
                                 </div>
                             </div>
                         </div>
-                        <!-- end card -->
+                        <!-- End Row -->
+                        <div class="row">
+                            <div class="col-lg-12 text-end">
+                                <a href="{{route('item.index')}}" class="btn btn-secondary">Back</a>
+                                <button type="submit" class="btn btn-primary" form="itemForm">Submit</button>
+                            </div>
+                        </div>
                     </div>
-                    <!-- end col -->
+                    <!-- end card -->
                 </div>
-                <!-- end row -->
+                <!-- end col -->
             </div>
-        </form>
+            <!-- end row -->
+        </div>
     </div>
 </section>
 @endsection
@@ -156,6 +158,7 @@
 @push('js')
 @include('sweetalert::alert')
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/dropzone/4.2.0/min/dropzone.min.js"></script>
 <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/tagmanager/3.0.2/tagmanager.min.js"></script>
 <script src="https://code.jquery.com/jquery-3.5.1.js"></script>
 <script src="https://cdn.datatables.net/1.13.1/js/jquery.dataTables.min.js"></script>
@@ -191,6 +194,44 @@
         }).on("select2:close",()=>{
             $(".input-tags .selection").css("background","rgba(239, 239, 239, 0.5)")
         })
+
+
+        Dropzone.options.imageUpload = {
+            maxFilesize: 100,
+            renameFile: function(file) {
+                var dt = new Date();
+                var time = dt.getTime();
+                return time + file.name;
+            },
+            acceptedFiles: ".jpeg,.jpg,.png,.gif,.pdf,.zip",
+            addRemoveLinks: true,
+            timeout: 50000,
+            removedfile: function(file) {
+                var name = file.name;
+                console.log(file.name);
+                $.ajaxSetup({
+                        headers: {
+                            'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                        }
+                    }),
+                    $.ajax({
+                        type: 'POST',
+                        url: '{{url("dropzone/delete")}}',
+                        data: {
+                            filename: name
+                        },
+                        success: function(data) {
+                            console.log("File has been successfully removed!!");
+                        },
+                        error: function(xhr, ajaxOptions, thrownError) {
+                            console.log(xhr.responseText);
+                        },
+                    });
+                var fileRef;
+                return (fileRef = file.previewElement) != null ?
+                    fileRef.parentNode.removeChild(file.previewElement) : void 0;
+            },
+        };
     })
 </script>
 @endpush
